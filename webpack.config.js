12231d3df21ea28c0838501ad631343ed7e3974b
rojaji <robert.ojaji@gmail.com>
2024-02-10 21:45:34 -0800

    update package.json & webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: 'button.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'DadsWebsite'),
   },
   module: {
     rules: [
